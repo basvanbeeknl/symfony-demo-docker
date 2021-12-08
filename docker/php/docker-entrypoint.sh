6572c4a5603b8b59eef222f1a78d66d33b5d4580
Bas van Beek <bas@bvbmedia.nl>
2021-12-08 18:38:17 +0100

    Added composer update

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -32,6 +32,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	if [ "$APP_ENV" != 'prod' ]; then
 		rm -f .env.local.php
 		composer install --prefer-dist --no-progress --no-interaction
+		composer update
 	fi
 
 	if grep -q ^DATABASE_URL= .env; then
